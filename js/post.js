56e97556dfe8cf303abab7ef4dbbfa2704db5d26
Kwonyujin04 <2310867@sookmyung.ac.kr>
2024-09-26 17:16:34 +0900

    Feat: 디자인 변경 및 커뮤니티 추가 	new file:   BDB_community.html 	modified:   css/BDB.css 	new file:   css/BDB_community.css 	modified:   index.html 	new file:   js/post.js 	new file:   "\354\235\264\353\257\270\354\247\200/BDB_\353\201\274\353\201\274.png" 	new file:   "\354\235\264\353\257\270\354\247\200/\355\217\254\354\212\244\355\212\2701.jpeg" 	new file:   "\354\235\264\353\257\270\354\247\200/\355\231\210\354\235\264\353\257\270\354\247\200_\354\203\235\354\235\274_\354\266\225\355\225\2302.png"

---
+++ js/post.js
@@ -0,0 +1,14 @@
+const post = [
+    {
+        userName: "권유진",
+        postBody: "안녕하세요.",
+        postImg: "/이미지"
+    },
+    {
+        userName: "권유진",
+        postBody: "안녕하세요.",
+        postImg: "/이미지"
+    }
+];
+
+
