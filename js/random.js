03a2c4e34172d949f96963ff8e9e5c7c0ec4a7bf
Kwonyujin04 <2310867@sookmyung.ac.kr>
2025-03-05 10:36:23 +0900

    feat: ramdom modify

--- js/random.js
+++ js/random.js
@@ -1,5 +1,6 @@
 function getRandom(max, win) {
     const getComp = localStorage.getItem("getComp");
+    const itemCount = localStorage.getItem("itemCount");
 
     if (getComp === "true") {
         alert("이미 참여하셨습니다. ");
@@ -10,10 +11,16 @@ function getRandom(max, win) {
     const top = max - win;
 
     if (rd >= top) {
-        alert("끼끼의 선물 당첨입니다! 설명을 참고하시고 메일을 보내면 추첨을 통해 선물을 드립니다. (택배 가능)");
+        alert("끼끼의 선물 당첨입니다! 설명을 참고하시고 메일을 보내면 선물을 드립니다. ");
+        itemCount++;
+
     }
     else {
         alert("다음 기회에! ");
+    }
+
+    if (itemCount == 2) {
+        top = 10000;
     }
 
     localStorage.setItem("likeCount", "false");
