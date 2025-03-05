8c8e33394ef26907130f34dd274f2d75e1cac41a
Kwonyujin04 <2310867@sookmyung.ac.kr>
2025-03-05 11:33:00 +0900

    fix

--- js/random.js
+++ js/random.js
@@ -1,6 +1,5 @@
 function getRandom(max, win) {
     const getComp = localStorage.getItem("getComp");
-    const itemCount = localStorage.getItem("itemCount");
 
     if (getComp === "true") {
         alert("이미 참여하셨습니다. ");
@@ -12,15 +11,9 @@ function getRandom(max, win) {
 
     if (rd >= top) {
         alert("끼끼의 선물 당첨입니다! 설명을 참고하시고 메일을 보내면 선물을 드립니다. ");
-        itemCount++;
-
     }
     else {
         alert("다음 기회에! ");
-    }
-
-    if (itemCount == 2) {
-        top = 10000;
     }
 
     localStorage.setItem("likeCount", "false");
